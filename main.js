b337b4bb79f2e205668be50189181d8f13eacbb0
JIANGTAO <jiang_tao@cmu.ac.th>
2021-07-02 09:40:22 +0800

    Bind the data to the attribute value
    Add new link bind to product name

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return {
             product:'Boots',
-            description:'This is boots description'
+            description:'This is boots description',
+            image:'./assets/images/socks_green.jpg',
+            link:'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }
 })
