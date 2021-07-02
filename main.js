1b7a81db2abb5e2d5c45d33bba157687152d84ae
JIANGTAO <jiang_tao@cmu.ac.th>
2021-07-02 11:09:50 +0800

    Array
    Put in the single line

--- main.js
+++ main.js
@@ -7,7 +7,18 @@ const app = Vue.createApp({
             link:'https://www.camt.cmu.ac.th/index.php/th/',
             inStock:true,
             inventory:10,
-            onSale:false
+            onSale:false,
+            details:['50% cotton','30% wool','20% polyester'],
+            variants:[
+                {id : 2234,color: 'green'},
+                {id : 2235,color: 'blue'}
+            ],
+            sizes:[
+                {id : 2236,size: 'S'},
+                {id : 2237,size: 'M'},
+                {id : 2238,size: 'L'}
+            ]
+            
         }
     }
 })
