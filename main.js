61834f65243aa61579365f5e715242a12bb31c13
JIANGTAO <jiang_tao@cmu.ac.th>
2021-07-02 11:57:46 +0800

    Last
    button to change

--- main.js
+++ main.js
@@ -10,16 +10,29 @@ const app = Vue.createApp({
             onSale:false,
             details:['50% cotton','30% wool','20% polyester'],
             variants:[
-                {id : 2234,color: 'green'},
-                {id : 2235,color: 'blue'}
+                {id : 2234,color: 'green', image: './assets/images/socks_green.jpg'},
+                {id : 2235,color: 'blue' , image: './assets/images/socks_blue.jpg'}
             ],
             sizes:[
                 {id : 2236,size: 'S'},
                 {id : 2237,size: 'M'},
                 {id : 2238,size: 'L'}
-            ]
-            
+            ],
+            cart:0        
+        }
+        
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        changeStatus(){
+            this.inStock=false
         }
     }
 })
+
 
